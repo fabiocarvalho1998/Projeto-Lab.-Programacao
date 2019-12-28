bd5925a6fb324019e1353ab54e1d846392de6cde
JsBraz <theepicgamer000@gmail.com>
2019-12-28 20:44:35 +0000

    pagina principal listagens

--- musicStore/app/Http/Controllers/HomeController.php
+++ musicStore/app/Http/Controllers/HomeController.php
@@ -1,14 +1,27 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use DB;
 use Illuminate\Http\Request;
+use Session;
+
 
 class HomeController extends Controller{
 
     public function index(){
 
+        $this->AdminAuthCheck();
+        $all_produtos_publicados=DB::table('produtos')
 
+            ->join('categorias', 'produtos.cat_id','=','categorias.cat_id')
+            ->join('marcas','produtos.m_id','=','marcas.m_id')
+            ->select('produtos.*','categorias.cat_name','marcas.m_name')
+
+            ->get();
+
+
+        $manage_produtos_publicados=view('pages.home')->with('all_produtos_publicados',$all_produtos_publicados);
+        return view('layout')->with('pages.home',$manage_produtos_publicados);
         //return view('pages.home');
     }
 }
